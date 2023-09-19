c82b65ce03f1f39368178e506d95248184b9d6e1
heliosalesjr <heliosalesjr@gmail.com>
2023-09-19 17:17:32 -0300

    reaload delay + bullet direction & cleaning when ofscreen

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,12 @@
 extends AnimatedSprite2D
 
 var velocity = Vector2(0,0)
+var can_shoot = true
 
 const SPEED = Vector2(125, 90)
 const Bullet = preload("res://scenes/player_bullet.tscn")
+
+@onready var reload_timer = $ReloadTimer
 
 func _process(delta):
 	velocity.x = Input.get_axis("move_left", "move_right")
@@ -18,10 +21,20 @@ func _process(delta):
 	else:
 		pass
 	
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
 		bullet_instance.global_position = global_position
 		get_tree().current_scene.add_child(bullet_instance)
+		
+		if flip_h:
+			bullet_instance.velocity.x = -1
+			bullet_instance.flip_h = true
+			
+		reload_timer.start()
+		can_shoot = false
 
 func _physics_process(delta):
 	global_position += velocity * SPEED  * delta
+
+func _on_reload_timer_timeout():
+	can_shoot = true
--- scenes/player_bullet.gd
+++ scenes/player_bullet.gd
@@ -6,3 +6,6 @@ const SPEED = 300
 
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
