abecf42299af2337e21a84be85f9969c0b5fdc64
heliosalesjr <heliosalesjr@gmail.com>
2023-09-18 21:10:41 -0300

    player movement + direction

---
+++ scenes/player.gd
@@ -0,0 +1,22 @@
+extends AnimatedSprite2D
+
+var velocity = Vector2(0,0)
+const SPEED = Vector2(125, 90)
+
+func _process(delta):
+	velocity.x = Input.get_axis("move_left", "move_right")
+	velocity.y = Input.get_axis("move_up", "move_down")
+	
+	velocity = velocity.normalized()
+	
+	if velocity.x > 0:
+		flip_h = false
+	elif velocity.x < 0:
+		flip_h = true
+	else:
+		pass
+	
+	
+
+func _physics_process(delta):
+	global_position += velocity * SPEED  * delta
