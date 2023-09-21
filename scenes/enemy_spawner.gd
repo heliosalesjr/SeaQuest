05111271a13212f3eed4d8a7064945b536392213
heliosalesjr <heliosalesjr@gmail.com>
2023-09-21 20:27:55 -0300

    spawning direction + randomness

--- scenes/enemy_spawner.gd
+++ scenes/enemy_spawner.gd
@@ -1,8 +1,22 @@
 extends Marker2D
 
+@export var facing_left = false
+
 const Shark = preload("res://scenes/shark.tscn")
 
+@onready var timer = $Timer
+
+
+func _ready():
+	timer.wait_time = randf_range(1,5)
+	timer.start()
+	
 func _on_timer_timeout():
 	var shark_instance = Shark.instantiate()
 	get_tree().current_scene.add_child(shark_instance)
 	shark_instance.global_position = global_position
+	
+	if facing_left:
+		shark_instance.flip_direction()
+
+	
--- scenes/shark.gd
+++ scenes/shark.gd
@@ -1,16 +1,20 @@
 extends Area2D
 
-const SPEED = 100
+const SPEED = 50
 const MOVEMENT_FREQUENCY = 0.1
 const MOVEMENT_AMPLITUDE = 0.3
 
 var velocity = Vector2(1,0)
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	velocity.y = sin(global_position.x * MOVEMENT_FREQUENCY) * MOVEMENT_AMPLITUDE
 	global_position += velocity * SPEED * _delta
 
+func flip_direction():
+	velocity = -velocity
+	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
 
 
 func _on_area_entered(area):
