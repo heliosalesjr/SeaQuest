32822d759b53b3615cd40a4ae8ac4da5b1f0cb78
heliosalesjr <heliosalesjr@gmail.com>
2023-09-21 17:52:37 -0300

    enemy spawning system

---
+++ scenes/enemy_spawner.gd
@@ -0,0 +1,8 @@
+extends Marker2D
+
+const Shark = preload("res://scenes/shark.tscn")
+
+func _on_timer_timeout():
+	var shark_instance = Shark.instantiate()
+	get_tree().current_scene.add_child(shark_instance)
+	shark_instance.global_position = global_position
--- scenes/shark.gd
+++ scenes/shark.gd
@@ -17,3 +17,7 @@ func _on_area_entered(area):
 	if area.is_in_group("PlayerBullet"):
 		area.queue_free()
 		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
